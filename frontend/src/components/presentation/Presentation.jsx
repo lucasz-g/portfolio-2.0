2030c4535eaedc76d5ca9ea58b17c82d71c4b22a
Lucas Eduardo Garcia <evosystem02.ti@wheatonbrasil.com.br>
2026-07-02 09:00:50 -0300

    Update: react bits components

--- frontend/src/components/presentation/Presentation.jsx
+++ frontend/src/components/presentation/Presentation.jsx
@@ -1,13 +1,33 @@
 import presentationBackground from "../../assets/presentation-background.mp4"
 import { ScrollArrow } from "../scrollArrow/ScrollArrow"
 import "./presentation.css"
+import SoftAurora from '../../react-bits/SoftAurora';
 
 export const Presentation = () => {
     return (
         <section id="home" className="presentation-section">
-            <video className="presentation-video" autoPlay muted loop playsInline>
+            {/* <video className="presentation-video" autoPlay muted loop playsInline>
                 <source src={presentationBackground} type="video/mp4" />
-            </video>
+            </video> */}
+
+            <div className="presentation-background">
+                <SoftAurora
+                    speed={0.6}
+                    scale={1.5}
+                    brightness={1}
+                    color1="#f7f7f7"
+                    color2="#e100ff"
+                noiseFrequency={2.5}
+                noiseAmplitude={1}
+                bandHeight={0.5}
+                bandSpread={1}
+                octaveDecay={0.1}
+                layerOffset={0}
+                colorSpeed={1}
+                enableMouseInteraction
+                mouseInfluence={1}
+            />
+            </div>
 
             <div className="role-name-div">
                 <p className="role">software engineer</p>
